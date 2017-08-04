47639f78b80c82947757f9656eaad6958d8092eb
mslmnb <mslmn@rambler.ru>
2017-08-04 22:10:31 +0600

    Подготовка к деплою на Heroku. Добавлены профили

---
+++ hr.bat
@@ -0,0 +1,2 @@
+call mvn -B -s settings.xml -DskipTests=true clean package
+call java -Dspring.profiles.active="heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.war
